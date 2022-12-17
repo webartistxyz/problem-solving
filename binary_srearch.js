ee0836a9991ba293969cbabe51f2eb7e8e27a635
webartistxyz <tasnif06@gmail.com>
2022-12-18 01:34:41 +0600

    updated binary search

--- binary_srearch.js
+++ binary_srearch.js
@@ -7,14 +7,14 @@ function binary_search(arr, target) {
     while (left <= right) {
         mid = Math.ceil((left + right) / 2);
 
-        if (arr[mid] === target){
-          return mid;
+        if (arr[mid] === target) {
+            return mid;
         }
 
-        if (arr[mid] < target){
-          left = mid + 1;
-        }else {
-          right = mid -1;
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
     return -1;
