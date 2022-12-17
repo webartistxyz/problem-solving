393dbecd0381998d4a4782872800f4d862a26ec5
webartistxyz <tasnif06@gmail.com>
2022-12-18 01:21:22 +0600

    updated binary search

--- binary_srearch.js
+++ binary_srearch.js
@@ -1,26 +1,26 @@
-function binary_serach() {
-  let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let x = 7;
+function binary_search(arr, target) {
+    let left = 0;
+    let right = arr.length;
 
-  let mid = 0;
+    let mid = 0;
 
-  let left = 0;
-  let right = 9;
+    while (left <= right) {
+        mid = Math.ceil((left + right) / 2);
 
-  while (left <= right) {
-    mid = Math.ceil((left + right) / 2);
+        if (arr[mid] === target){
+          return mid;
+        }
 
-    if (a[mid] == x) {
-      return mid;
+        if (arr[mid] < target){
+          left = mid + 1;
+        }else {
+          right = mid -1;
+        }
     }
-
-    if (a[mid] < x) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  return -1;
+    return -1;
 }
 
-console.log(binary_serach());
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let target = 10;
+
+console.log(binary_search(arr, target));
